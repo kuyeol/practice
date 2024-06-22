3c8394003f7724f7c8aff6064cdd60a53ccaea62
dev <125532184+kuyeol@users.noreply.github.com>
2024-06-22 09:01:52 +0900

    Create Controller.java

---
+++ code-with-quarkus/src/main/java/org/acme/Controller.java
@@ -0,0 +1,13 @@
+
+public class Controller{
+
+@Inject
+FXMLLoader fxmlLoader;
+
+public void load() {
+  Parent root = this.fxmlLoader.load(this.getClass().getResourceAsStream("/app.fxml"));
+  Scene scene = new Scene(root);
+  stage.setScene(scene);
+  stage.show();
+}
+}
